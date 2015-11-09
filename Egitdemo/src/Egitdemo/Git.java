57b2a86c05f876397953cb76be5de39a56d07c72
FA12-BSE-264 <FA12-BSE-264@LAB7B-08.cns.ciit>
2015-11-09 13:42:33 +0500

    Yai Lo project tumhara hoa

---
+++ Egitdemo/src/Egitdemo/Git.java
@@ -0,0 +1,11 @@
+package Egitdemo;
+
+import javax.print.DocFlavor.STRING;
+
+public class Git 
+{
+	public static void main(STRING[] arg) 
+	{
+System.out.println("Lets do something using git");
+	}
+	}
